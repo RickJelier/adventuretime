2a7235847e926df2cd300fe42ccac2fc39803352
Rick Jelier <rick@hollandshielding.com>
2016-03-10 12:50:50 +0100

    Made a 'GameStage' class to handle all of the gamestate and gamestage logic

---
+++ AdventureTime/GameStage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventureTime
+{
+    class GameStage
+    {
+        private string areaName;
+        public static string currentArea;
+
+        public GameStage(string areaName)
+        {
+            this.areaName = areaName;
+        }
+
+        public void dialog(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public string ask(List<string> allQuestions)
+        {
+            foreach (string question in allQuestions)
+            {
+                int optionNumber = allQuestions.IndexOf(question) + 1;
+                Console.WriteLine(optionNumber + " - " + question);
+            }
+
+            return Console.ReadLine();
+        }
+
+        public void enter()
+        {
+            currentArea = areaName;
+        }
+
+        public void confirm()
+        {
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
